00cbef5f4ba36862f7942913f41834fd759507b5
Alan Wynn <alan.wynn@me.com>
2014-09-25 14:10:41 +0100

    update license and add php5.6 for travis

--- src/InvalidNumberException.php
+++ src/InvalidNumberException.php
@@ -1,4 +1,26 @@
 <?php
 namespace CloudDataService\NHSNumberValidation;
 
+/**
+ * NHS Number Validation Class
+ * Copyright (C) 2014  Cloud Data Service
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package cds/nhs-number-validation
+ * @author Alan Wynn <alan@clouddataservice.co.uk>
+ * @license AGPL v3
+ */
+
 class InvalidNumberException extends \Exception {}
